ba96e644850d451943123abbdd81378b2a2dcecb
andreagavardi <andreagavardi89@gmail.com>
2021-06-08 16:19:02 +0200

    completato milestone 2

--- milestone_2/assets/js/main.js
+++ milestone_2/assets/js/main.js
@@ -2,19 +2,31 @@
     el: '#app',
     data:{
         API_key:"ee51577542fc46315161e476972d3102",
-        url:"https://api.themoviedb.org/3/search/movie?api_key=",
+        url:"https://api.themoviedb.org/3/",
+        linkMovies:"search/movie",
+        linkTvShow:"search/tv",
         query:null,
         films:null,
+        tvShows:null,
         ImgUrl:"https://www.countryflags.io/"
      },
     methods:{   
         cercaFilm(){
             axios
-            .get(this.url+this.API_key+"&language=it-IT&query="+this.query)
+            .get(this.url+this.linkMovies+"?api_key="+this.API_key+"&language=it-IT&query="+this.query)
             .then(resp=>{
                 if(this.query!=null){
                     this.films=resp.data.results
                     console.log(this.films);
+                    
+                }
+            });
+            axios
+            .get(this.url+this.linkTvShow+"?api_key="+this.API_key+"&language=it-IT&query="+this.query)
+            .then(resp=>{
+                if(this.query!=null){
+                    this.tvShows=resp.data.results
+                    console.log(this.tvShows);
                     
                 }
             });
