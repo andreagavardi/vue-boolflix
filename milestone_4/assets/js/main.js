16ecec67a8e4009a4f902f9d8a67a06756456daa
andreagavardi <andreagavardi89@gmail.com>
2021-06-09 17:59:31 +0200

    terminato milestone_4 responsive

--- milestone_4/assets/js/main.js
+++ milestone_4/assets/js/main.js
@@ -17,8 +17,8 @@
         
      },
     methods:{   
-        getRatings:function(obj){
-                return Math.round(obj / 2);
+        getRatings:function(vote){  //metodo a cui passo la proprietà "vote_average" che poi divido per 2 
+                return Math.round(vote / 2); //(per avere il massimo di 5) e lo arrotondo
             
             },
         cercaFilm(){
@@ -28,12 +28,12 @@
             .then(resp=>{
                 if(this.query!=""){
                     this.films=resp.data.results;
-                    this.emptyFilms="";
+                    this.emptyFilms=""; //azzero i vari errori così da non mostrarli nella nuova ricerca
                     this.error=null
                     /* console.log(this.films); */
                    
                 }
-                if(this.films.length==0){
+                if(this.films.length==0){ //se la ricerca non ha risultati
                     this.emptyFilms='La ricerca: "' + this.query +'" non ha prodotto nessun risultato' 
                 }
             })
@@ -41,18 +41,19 @@
                 console.error(error);
                 this.error='OOOPS QUALCOSA È ANDATO STORTO: ' +error;
             });
+            /* ricerca Tv Series */
             axios
             .get(this.url+this.linkTvShow+"?api_key="+this.API_key+"&language=it-IT&query="+this.query)
             .then(resp=>{
                 if(this.query!=null){
                     this.tvShows=resp.data.results
                     console.log(this.tvShows);
-                    this.emptyTvShows="";
+                    this.emptyTvShows=""; //azzero i vari errori così da non mostrarli nella nuova ricerca
                     this.error_2=null
 
                     
                 }
-                if(this.tvShows.length==0){
+                if(this.tvShows.length==0){ //se la ricerca non ha risultati
                     this.emptyTvShows='La ricerca: "' + this.query +'" non ha prodotto nessun risultato'
                 }
             })
